7ab9de116b24e00efd5e8af2f18b07fff42c72dd
squirrelcoding <rosilescarlos28@gmail.com>
2025-09-27 13:00:44 -0500

    got wiener process

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from quant!")
+import numpy as np
+import pandas as pd
 
+
+def main():
+    points = [0.0]
+    for i in range(1, 500):
+        points.append(points[i] + np.random.normal(0.0, 1.0))
+    
+    
 
 if __name__ == "__main__":
     main()
